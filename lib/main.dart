cadfac9dd1d51259f23dfabbbba12c257933d84b
archit27-uo <archit27chawda@gmail.com>
2023-09-03 23:48:29 +0530

    newtask screen added

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: '/second',
       routes: {
         '/': (context) => TaskScreen(),
         '/second': (context) => const newTask(),
--- lib/screens/newTask_screen.dart
+++ lib/screens/newTask_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:day_night_time_picker/day_night_time_picker.dart';
 
 class newTask extends StatefulWidget {
   const newTask({Key? key}) : super(key: key);
@@ -9,8 +9,97 @@ class newTask extends StatefulWidget {
 }
 
 class _newTaskState extends State<newTask> {
+  Time _time = Time(hour: 24, minute: 0, second: 0);
+  void onTimeChanged(Time newTime) {
+    setState(() {
+      _time = newTime;
+      print(_time);
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                SizedBox(),
+                Text(
+                  'New Task',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+
+                Container(
+                  child: Icon(
+                    Icons.close,
+                  ),
+                )
+              ],
+            ),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Title',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'What are you planning?',
+                  ),
+                ),
+              ),
+            ),
+            TextButton(
+                onPressed: (){
+                  Navigator.of(context).push(
+                    showPicker(
+
+                      context: context,
+                      value: _time,
+                      iosStylePicker: true,
+                      sunrise: TimeOfDay(hour: 6, minute: 0), // optional
+                      sunset: TimeOfDay(hour: 18, minute: 0), // optional
+                      duskSpanInMinutes: 120, // optional
+                      onChange: onTimeChanged,
+                    ),
+                  );
+                },
+                child: Text('Select Time')
+            ),
+            Container(
+              margin: EdgeInsetsDirectional.only(end: 0),
+              child: Container(
+
+                height: 50,
+                color: Colors.redAccent,
+                child: Center(
+                  child: Text(
+                    'Create',
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
